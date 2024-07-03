6ca122502eb44c7c11089e31b156909f28113b70
busyfingers <n.malmqvist@gmail.com>
2024-07-03 23:01:45 +0200

    feat: Port and TLS config options, support for sending file path

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import path from 'path'
 import express, { Request, Response } from 'express'
 import { Client } from 'basic-ftp'
 import 'dotenv/config'
+import { DownloadResult } from './types'
 
 const app = express()
 app.use(express.json())
@@ -14,27 +16,60 @@ app.post('/', async (req: Request, res: Response) => {
   }
 
   try {
-    await downloadFile(fileName, res)
+    const result = await downloadFile(fileName, res)
+
+    if (!result.success) {
+      return res.status(400).send(result.error)
+    }
   } catch (error) {
     return res.status(500).send()
   }
 })
 
-async function downloadFile(fileName: string, writeStream: Response) {
+async function downloadFile(
+  fullFilePath: string,
+  writeStream: Response
+): Promise<DownloadResult> {
   const client = new Client()
+  const secure =
+    process.env.TLS === 'explicit'
+      ? true
+      : process.env.TLS === 'implicit'
+      ? 'implicit'
+      : false
 
   try {
     await client.access({
       host: process.env.FTP_HOST,
       user: process.env.FTP_USER,
       password: process.env.FTP_PASSWORD,
+      port: parseInt(process.env.FTP_PORT ?? '21'),
+      secure,
     })
+
+    const { dir: filePath, base: fileName } = path.parse(fullFilePath)
+
+    if (filePath) {
+      console.log('changing dir')
+      await client.cd(filePath)
+    }
+
+    const files = await client.list()
+    const match = files.some((f) => f.name === fileName)
+
+    if (!match) {
+      return { success: false, error: `Unable to find file '${fileName}'` }
+    }
+
     await client.downloadTo(writeStream, fileName)
+
+    return { success: true }
   } catch (err) {
-    console.log(err)
+    console.log('err', err)
     throw err
+  } finally {
+    client.close()
   }
-  client.close()
 }
 
 app.listen(port, () => {
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type DownloadResult = {
+  success: Boolean
+  error?: string
+}
